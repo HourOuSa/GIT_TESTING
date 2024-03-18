57bf69f6a4f0919bf8102af962451f8631c3fe28
VuthMenghuor <hourousa9@gmail.com>
2024-03-19 00:54:21 +0700

    second commit

--- test.java
+++ test.java
@@ -3,7 +3,7 @@ package GIT_TESTING;
 public class test {
 
     public static void main(String[] args) {
-        System.out.println("Hello bro");
+        System.out.println("Hello bro vuth menghuor");
     }
 }
 
